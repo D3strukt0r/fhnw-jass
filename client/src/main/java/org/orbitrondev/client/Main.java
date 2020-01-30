c887fc3ae25fffad9537726827925231519e851a
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-01-30 18:11:56 +0100

    Turn example into JavaFX example

--- client/src/main/java/org/orbitrondev/client/Main.java
+++ client/src/main/java/org/orbitrondev/client/Main.java
@@ -1,10 +1,29 @@
 package org.orbitrondev.client;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.orbitrondev.lib.Message;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        Message m = new Message("Hello, I'm the client!");
-        System.out.println(m.getMessage());
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Message m = new Message("Hello World!");
+
+        primaryStage.setTitle(m.getMessage());
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
