2e6d5ad0aafcd9eb4e28d9d290420ddbf1fe5dce
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-01-31 11:15:49 +0100

    Add ServiceLocator

---
+++ lib/src/main/java/org/orbitrondev/jass/lib/ServiceLocator/Service.java
@@ -0,0 +1,7 @@
+package org.orbitrondev.jass.lib.ServiceLocator;
+
+/* https://www.geeksforgeeks.org/service-locator-pattern/ */
+
+interface Service {
+    public String getName();
+}
---
+++ lib/src/main/java/org/orbitrondev/jass/lib/ServiceLocator/ServiceLocator.java
@@ -0,0 +1,31 @@
+package org.orbitrondev.jass.lib.ServiceLocator;
+
+/* https://www.geeksforgeeks.org/service-locator-pattern/ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceLocator {
+    private static List<Service> services = new ArrayList<>();
+
+    public static Service get(String serviceName) {
+        for (Service service : services) {
+            if (service.getName().equalsIgnoreCase(serviceName)) {
+                return service;
+            }
+        }
+        return null;
+    }
+
+    public static void add(Service newService) {
+        boolean exists = false;
+        for (Service service : services) {
+            if (service.getName().equalsIgnoreCase(newService.getName())) {
+                exists = true;
+            }
+        }
+        if (!exists) {
+            services.add(newService);
+        }
+    }
+}
