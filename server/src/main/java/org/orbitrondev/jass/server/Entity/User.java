d66f02d1571ab08a90168a6d35bf62f9b81eaa28
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-02-11 21:59:37 +0100

    Add missing setter for the user entity

--- server/src/main/java/org/orbitrondev/jass/server/Entity/User.java
+++ server/src/main/java/org/orbitrondev/jass/server/Entity/User.java
@@ -95,6 +95,10 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
     }
