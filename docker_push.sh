995fb4f56d24311f13205e63d3e2e67341fff6f1
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-02-24 19:03:58 +0100

    Fix docker upload script

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# Build
+docker build -t jass .
+
+# Upload
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push d3strukt0r/fhnw-jass
+
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+      DOCKER_PUSH_TAG="latest"
+else
+      DOCKER_PUSH_TAG="nightly"
+fi
+
+docker tag back "$DOCKER_USERNAME"/fhnw-jass:"$DOCKER_PUSH_TAG"
+docker push "$DOCKER_USERNAME"/fhnw-jass:"$DOCKER_PUSH_TAG"
