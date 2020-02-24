af398193bd1135ac12364b193fdb2fd24f163573
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-02-24 19:08:52 +0100

    Fix docker upload script

--- docker_push.sh
+++ docker_push.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Login to make sure we have access to private dockers
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 # Build
 docker build -t jass .
 
 # Upload
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
       DOCKER_PUSH_TAG="latest"
@@ -13,5 +14,5 @@ else
       DOCKER_PUSH_TAG="nightly"
 fi
 
-docker tag back "$DOCKER_USERNAME"/fhnw-jass:"$DOCKER_PUSH_TAG"
+docker tag jass "$DOCKER_USERNAME"/fhnw-jass:"$DOCKER_PUSH_TAG"
 docker push "$DOCKER_USERNAME"/fhnw-jass:"$DOCKER_PUSH_TAG"
