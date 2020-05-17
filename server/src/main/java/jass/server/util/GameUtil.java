c576fc132ea4a082435c5e0f792786d5005213e3
Vic <victor.hargrave@students.fhnw.ch>
2020-05-17 17:23:32 +0200

    add show card timer

--- server/src/main/java/jass/server/util/GameUtil.java
+++ server/src/main/java/jass/server/util/GameUtil.java
@@ -358,8 +358,7 @@ public final class GameUtil implements ChosenGameModeEventListener, PlayedCardEv
 
                 // start new turn after 4 seconds
                 if (turn.getWinningUser() != null) {
-                    // TODO set back to 4000
-                    Thread.sleep(1000);
+                    Thread.sleep(3500);
                     TurnEntity newTurn = addNewTurn(turn.getWinningUser(), currentRound);
 
                     BroadcastTurn newBroadcastTurn = new BroadcastTurn(new BroadcastTurnData(newTurn.getId(),
